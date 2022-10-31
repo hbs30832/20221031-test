5110075112003f7d911c34aa48a30013666741c3
hbs30832 <hbs9312@gmail.com>
2022-10-31 22:06:51 +0900

    주석 추가

--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ let slideBtnList = document.querySelectorAll(".slideBtnList button");
 
 let slideIdx = 0;
 
+// 세개의 버튼 클릭 시 각 인덱스 * 100%만큼 움직이게 적용
 slideBtnList.forEach((btn, idx) => {
   btn.addEventListener("click", () => {
     slideIdx = idx;
-    slideWrapper.style.transform = `translateX(${slideIdx * -100}vw)`;
+    slideWrapper.style.transform = `translateX(${slideIdx * -100}%)`;
   });
 });
