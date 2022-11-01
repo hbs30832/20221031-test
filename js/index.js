ce1f3c7b88ad347d65b62a9f5275122ba52160d5
hbs30832 <hbs9312@gmail.com>
2022-11-01 19:45:24 +0900

    slideIdx 값 삭제

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
 let slideWrapper = document.querySelector(".slideWrapper");
 let slideBtnList = document.querySelectorAll(".slideBtnList button");
 
-let slideIdx = 0;
-
 // 세개의 버튼 클릭 시 각 인덱스 * 100%만큼 움직이게 적용
 slideBtnList.forEach((btn, idx) => {
   btn.addEventListener("click", () => {
-    slideIdx = idx;
-    slideWrapper.style.transform = `translateX(${slideIdx * -100}%)`;
+    slideWrapper.style.transform = `translateX(${idx * -100}%)`;
   });
 });
